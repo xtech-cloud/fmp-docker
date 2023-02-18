08e30e4adf2b2cfe6614b1fec87369ad71a2e8d4
Niall Liu <niall@aliyun.com>
2023-02-18 10:44:05 +0800

    update

--- wsl/install-all.sh
+++ wsl/install-all.sh
@@ -1,7 +1,8 @@
 mkdir -p /fmp
 cp -rf ../fmp-web /fmp/
 cp -rf ../fmp-pmc /fmp/
-docker-compose -f ./docker-compose-dsc.yml -p fmp up -d
-docker-compose -f ./docker-compose-msa.yml -p fmp up -d
-docker-compose -f ./docker-compose-pmc.yml -p fmp up -d
-docker-compose -f ./docker-compose-web.yml -p fmp up -d
+docker-compose -f docker-compose-dsc.yml -p fmp up -d
+docker-compose -f docker-compose-msa.yml -p fmp up -d
+docker-compose -f docker-compose-pmc.yml -p fmp up -d
+docker-compose -f docker-compose-web.yml -p fmp up -d
+
